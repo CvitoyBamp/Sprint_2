a0b5e384271607d10c33504037d651a37ff8680d
CvitoyBamp <cvitoybamp@yandex.ru>
2022-05-01 09:50:38 +0300

    + Enclosed.class

--- src/test/java/com/example/AnimalTest.java
+++ src/test/java/com/example/AnimalTest.java
@@ -2,49 +2,56 @@ package com.example;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 
-@RunWith(Parameterized.class)
+import static org.junit.Assert.assertThrows;
+
+@RunWith(Enclosed.class)
 public class AnimalTest{
 
-    public String animalKind;
-    public List<String> mealList;
+    @RunWith(Parameterized.class)
+    public static class AnimalTestParameterized {
 
-    public AnimalTest(String animalKind, List<String> mealList) {
-        this.animalKind = animalKind;
-        this.mealList = mealList;
+        private String animalKind;
+        private List<String> mealList;
+
+        public AnimalTestParameterized(String animalKind, List<String> mealList) {
+            this.animalKind = animalKind;
+            this.mealList = mealList;
+        }
+
+        @Parameterized.Parameters
+        public static Object[][] getMealData() {
+            return new Object[][]{
+                    {"Травоядное", List.of("Трава", "Различные растения")},
+                    {"Хищник", List.of("Животные", "Птицы", "Рыба")},
+            };
+        }
+
+        @Test
+        public void checkAnimalCorrectMeal() throws Exception {
+            Animal animal = new Animal();
+            Assert.assertEquals("Ожидается совпадение блюд на покушать", mealList, animal.getFood(animalKind));
+        }
     }
 
-    @Parameterized.Parameters
-    public static Object[][] getMealData() {
-        return new Object[][] {
-                {"Травоядное", List.of("Трава", "Различные растения")},
-                {"Хищник", List.of("Животные", "Птицы", "Рыба")},
-        };
-    }
+    public static class AnimalTestSingle{
 
-    @Test
-    public void checkAnimalCorrectMeal() throws Exception{
-        Animal animal = new Animal();
-        Assert.assertEquals("Ожидается совпадение блюд на покушать", mealList, animal.getFood(animalKind));
-    }
+        @Test
+        public void checkAnimalInCorrectMeal() throws Exception{
+            Animal animal = new Animal();
+            assertThrows(Exception.class, () -> animal.getFood("Человек-редиска"));
+        }
 
-    @Test(expected = Exception.class)
-    public void checkAnimalInCorrectMeal() throws Exception{
-        Animal animal = new Animal();
-        Assert.assertEquals(new Exception("Неизвестный вид животного, используйте значение Травоядное или Хищник"), animal.getFood("Человек-редиска"));
+        @Test
+        public void shouldReturnListOfFamilies() {
+            Animal animal = new Animal();
+            String expectedFamily = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+            Assert.assertEquals(expectedFamily, animal.getFamily());
+        }
     }
-
-    @Test
-    public void shouldReturnListOfFamilies() {
-        Animal animal = new Animal();
-        String expectedFamily = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
-        Assert.assertEquals(expectedFamily, animal.getFamily());
-    }
-
 }
+
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,74 +1,77 @@
 package com.example;
 
-import org.hamcrest.Matcher;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.assertThrows;
 
-@RunWith(Parameterized.class)
+@RunWith(Enclosed.class)
 public class LionTest {
 
-    @Mock
-    Feline feline;
+    @RunWith(Parameterized.class)
+    public static class LionTestParameterized {
 
-    @Before
-    public void init() {
-        MockitoAnnotations.initMocks(this);
+        @Mock
+        private static Feline feline;
+
+        private String actualSex;
+        private boolean expectedSex;
+
+        public LionTestParameterized(String actualSex, boolean expectedSex) {
+            this.actualSex = actualSex;
+            this.expectedSex = expectedSex;
+        }
+
+        @Parameterized.Parameters
+        public static Object[][] getSexData() {
+            return new Object[][]{
+                    {"Самец", true},
+                    {"Самка", false},
+            };
+        }
+
+        @Test
+        public void checkManeOfLineCorrectSex() throws Exception {
+            Lion lion = new Lion(actualSex, feline);
+            Assert.assertEquals("Ожидается совпадение пола", expectedSex, lion.doesHaveMane());
+        }
     }
 
-    public String actualSex;
-    public boolean expectedSex;
+    @RunWith(MockitoJUnitRunner.class)
+    public static class LionTestSingle {
 
-    public LionTest(String actualSex, boolean expectedSex) {
-        this.actualSex = actualSex;
-        this.expectedSex = expectedSex;
-    }
+        @Mock
+        private Feline feline;
 
-    @Parameterized.Parameters
-    public static Object[][] getSexData() {
-        return new Object[][] {
-                {"Самец", true},
-                {"Самка", false},
-        };
-    }
-
-    @Test
-    public void checkManeOfLineCorrectSex() throws Exception{
-        Lion lion = new Lion(actualSex, feline);
-        Assert.assertEquals("Ожидается совпадение пола", expectedSex, lion.doesHaveMane());
-    }
-
-    //Спасибо! Получилось!
-    @Test
-    public void checkManeOfLineInCorrectSex(){
-        assertThrows(Exception.class, () -> new Lion("Трансгендер", feline));
-    }
+        //Спасибо! Получилось!
+        @Test
+        public void checkManeOfLineInCorrectSex() {
+            assertThrows(Exception.class, () -> new Lion("Трансгендер", feline));
+        }
 
 
-    @Test
-    public void shouldHaveKittens() throws Exception{
-        int expectedKitten = 1;
-        Lion lion = new Lion("Самка", feline);
-        Mockito.when(feline.getKittens()).thenReturn(1);
-        Assert.assertEquals("Ждём одну живность",expectedKitten, lion.getKittens());
-    }
+        @Test
+        public void shouldHaveKittens() throws Exception {
+            int expectedKitten = 1;
+            Lion lion = new Lion("Самка", feline);
+            Mockito.when(feline.getKittens()).thenReturn(1);
+            Assert.assertEquals("Ждём одну живность", expectedKitten, lion.getKittens());
+        }
 
-    @Test
-    public void shouldHaveCorrectListOfMeal() throws Exception{
-        Lion lion = new Lion("Самец", feline);
-        List<String> expected = List.of("Животные", "Птицы", "Рыба");
-        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-        Assert.assertEquals("Получен некорректный список продуктов питания для Хищника", expected, lion.getFood());
+        @Test
+        public void shouldHaveCorrectListOfMeal() throws Exception {
+            Lion lion = new Lion("Самец", feline);
+            List<String> expected = List.of("Животные", "Птицы", "Рыба");
+            Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+            Assert.assertEquals("Получен некорректный список продуктов питания для Хищника", expected, lion.getFood());
+        }
     }
 }
